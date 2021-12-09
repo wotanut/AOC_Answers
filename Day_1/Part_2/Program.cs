1175d6e5be39845e750d63e14c3c2697b7dfd4db
wotanut <75843086+wotanut@users.noreply.github.com>
2021-12-09 10:02:53 +0000

    Removed path directions + made code work

--- Day_1/Part_2/Program.cs
+++ Day_1/Part_2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,35 +12,72 @@ namespace AOC
         static void Main(string[] args)
         {
 
+
+            /// plan
+
+            // firstly it goes through input.txt and adds up three numbers
+            // writes this output to output.txt
+            // might just re use the python code that I wrote earlier to loop through output.txt and compare them
+
+
+
             // defining some key variables
-            int counter = 0;
             int current_number = 0;
-            int one_back = 0;
-            int two_back = 0;
-            int three_back = 0;
-            int first_three_total = 0;
-            bool need_to_repeat = false;
-            List<int> new_totals = new List<int>();
-            string textFile = @"C:\Users\blewittsh\source\repos\AOC\input.txt";
+            int for_each_loops = 0;
+            string textFile = 
+            string output = 
+            int first_loop = 0;
+            int second_loop = 0;
+            bool next = false;
 
 
             // Read the file and display it line by line.  
             foreach (string line in File.ReadLines(textFile))
             {
-                // go up three numbers adding them togethor then go down two and repeat, adding the current sum to a list
-                if (counter == 0 || counter == 1 || counter == 2)
+                StreamReader sr = new StreamReader(textFile);
+                
+
+                // can't get the nth line of a file so must loop through each line of the file to try and find it
+
+                int one = for_each_loops + 1;
+                int two = for_each_loops + 2;
+
+                foreach (string new_line in File.ReadLines(textFile))
                 {
-                    first_three_total = first_three_total + Convert.ToInt32(line);
-                    counter = counter + 1;
+                    // Console.WriteLine(new_line);
+                    if (first_loop == one)
+                    {
+
+                        one = Convert.ToInt32(new_line);
+                        second_loop = first_loop;
+                        next = true;
+                    }
+                    else if(next == true)
+                    {
+                        two = Convert.ToInt32(new_line);
+                        next = false;
+                    }
+                    else
+                    {
+                        first_loop = first_loop + 1;
+                        second_loop = second_loop + 1;
+                    }
                 }
-                else
-                {
 
-                }
+                first_loop = 0;
+                second_loop = 0;
+                Console.WriteLine(line + " + " + one + " + " + two);
 
+                current_number = Convert.ToInt32(line) + Convert.ToInt32(one) + Convert.ToInt32(two);
+                Console.WriteLine(current_number);
 
-                // System.Console.WriteLine(line);
-                counter = counter + 1;
+                string write = Convert.ToString(current_number);
+
+                StreamWriter sw = File.AppendText(output);
+                sw.WriteLine(write);
+                sw.Close();
+                for_each_loops = for_each_loops + 1;
+                // File.AppendText(output, write);
             }
 
             // stops the program from closing at the end
@@ -48,5 +85,5 @@ namespace AOC
             Console.ReadLine();
 
         }
-        }
+    }
 }
